ae71d221e39f28d22f8653ab5c087ed6fa0a0487
emlhed <emlhed-1@student.ltu.se>
2022-05-05 11:15:23 +0200

    Commit for the selemium project

---
+++ src/main/java/TestPlan.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestPlan {
+
+    private static final WebDriver driver = new ChromeDriver();
+    private static final String SEARCH_TERM = "ltu";
+
+    @BeforeSuite
+    public static void setUP() {
+        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+    }
+
+    @Test(testName = "search google")
+    public static void searchOnGoogle() throws InterruptedException{
+
+        //Expect erroer when testing
+        //String textFel = "Llkfsafaeelöfmaöksemftu";
+        driver.get(Utils.BASE_URL);
+        GoogleSearch search = new GoogleSearch(driver);
+        search.acptTerms();
+        Thread.sleep(2000);
+        search.enterSearchTerm(SEARCH_TERM);
+        Thread.sleep(2000);
+        search.clickSearch();
+        Assert.assertTrue(search.searchForStringInResult(SEARCH_TERM));
+        Thread.sleep(3000);
+    }
+
+    @AfterSuite
+    public static void cleanUp(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,5 @@
+// Configuration class for Form Automation project
+public class Utils {
+    final static String BASE_URL = "https://www.google.se/";
+  final static String CHROME_DRIVER_LOCATION = "chromedriver";
+}
