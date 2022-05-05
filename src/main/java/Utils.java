d4e944b2d87116afb942746c97a5370f2f2f1d4e
emlhed <emlhed-1@student.ltu.se>
2022-05-05 11:32:47 +0200

    committ filde

--- src/main/java/Utils.java
+++
@@ -1,5 +0,0 @@
-// Configuration class for Form Automation project
-public class Utils {
-    final static String BASE_URL = "https://www.google.se/";
-  final static String CHROME_DRIVER_LOCATION = "chromedriver";
-}
